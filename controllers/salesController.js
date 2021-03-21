dc173e54bda3c5c3baeea62b92c6c8a6ca5bb425
FlavioSugano <flavio.sugano@gmail.com>
2021-03-21 01:08:24 -0300

    Req 9

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,23 +1,37 @@
-const service = require('../services/salesServices');
+const salesService = require('../services/salesServices');
+const productsService = require('../services/productsService');
 const { SUCCESS, NOT_FOUND } = require('../dictionary/statusCode');
 const { saleNotFound, WrongIdSaleFormat } = require('../dictionary/errorMessages');
 
+const updateQuantity = async (itens, method) => {
+  itens.forEach(async ({ productId, quantity: saleQuantity }) => {
+    let { _id, name, quantity } = await productsService.productById(productId);
+    
+    if (method === 'POST') quantity = quantity - saleQuantity;
+    if (method === 'DELETE') quantity = quantity + saleQuantity;
+
+    await productsService.updateProductInfo(_id, name, quantity);
+  });
+};
+
 const createNewSale = async (req, res, next) => {
   const sale = req.body;
-  const saleCreated = await service.createSale(sale);
+  const saleCreated = await salesService.createSale(sale);
+
+  await updateQuantity(saleCreated.itensSold, req.method);
 
   return res.status(SUCCESS).json(saleCreated);
 };
 
 const getAll = async (req, res, next) => {
-  const allSales = await service.allSales();
+  const allSales = await salesService.allSales();
 
   return res.status(SUCCESS).json({ sales: allSales });
 };
 
 const getById = async (req, res, next) => {
   const { id } = req.params;
-  const sale = await service.saleById(id);
+  const sale = await salesService.saleById(id);
 
   if (!sale) {
     return next({
@@ -33,7 +47,7 @@ const updateSale = async (req, res) => {
   const { id } = req.params;
   const sale = req.body;
 
-  const saleUpdated = await service.updateSaleInfo(id, sale);
+  const saleUpdated = await salesService.updateSaleInfo(id, sale);
 
   return res.status(SUCCESS).json(saleUpdated);
 };
@@ -41,7 +55,7 @@ const updateSale = async (req, res) => {
 const deleteSale = async (req, res) => {
   const { id } = req.params;
 
-  const deletedSale = await service.deleteSaleInfo(id);
+  const deletedSale = await salesService.deleteSaleInfo(id);
 
   if (!deletedSale) {
     return next({
@@ -49,6 +63,8 @@ const deleteSale = async (req, res) => {
       ...WrongIdSaleFormat
     });
   }
+
+  await updateQuantity(deletedSale.itensSold, req.method);
 
   return res.status(SUCCESS).json(deletedSale);
 };
