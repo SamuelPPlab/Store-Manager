652dd44986071385bc23b33bea184d1f2aaa2cfd
Ana Higo <anahigo.arq@gmail.com>
2021-03-22 11:50:35 -0300

    products local test and lint, ok

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const productRouter = require('./src/controllers/productController');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productRouter);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
---
+++ src/middleware/productMiddleware.js
@@ -0,0 +1,57 @@
+const { status, errors, codeStatus } = require('../schemas/status');
+const rescue = require('express-rescue');
+
+// Functions
+const theLengthLessThan = (value, number) => (!value || value.length < number);
+const isBiggerThan = (value, number) => (!value || value <= number);
+const isTypeIs = (value, typeOfValue) => (!value || typeof value !== typeOfValue);
+
+const validateName = rescue((req, res, next) => {
+  const { name } = req.body;
+
+  const minLength = 5;
+  
+  if (!name || theLengthLessThan(name, minLength)) {
+    return res.status(status.unprocessableEntity)
+      .json({ err: { code: codeStatus.invalidData, message: errors.nameLength }});
+  }
+
+  next();
+});
+
+const validateQuantity = rescue((req, res, next) => {
+  const { quantity } = req.body;
+
+  const minValue = 0;
+  if (isBiggerThan(quantity, minValue)) {
+    return res.status(status.unprocessableEntity)
+      .json({ err:{ code: codeStatus.invalidData, message: errors.qttBiggerOne }});
+  }
+  
+  if (isTypeIs(quantity, 'number')) {
+    return res.status(status.unprocessableEntity)
+      .json({ err: { code: codeStatus.invalidData, message: errors.qttMustBeNumber }});
+  }
+
+  next();
+});
+
+const validateId = rescue((req, res, next) => {
+  const { id } = req.params;
+
+  const idLength = 24;
+
+  if (!id || id.length !== idLength) {
+    return res.status(status.unprocessableEntity)
+      .json({ err: { code: codeStatus.invalidData, message: errors.idFormat }});
+  };
+
+
+  next();
+});
+
+module.exports = {
+  validateName,
+  validateQuantity,
+  validateId,
+};
