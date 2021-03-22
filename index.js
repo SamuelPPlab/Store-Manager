da2b7fcdf4b2263f9fa2db60e2b2db595cc34763
PAULO LINS <paulolins100@gmail.com>
2021-03-22 18:34:10 -0300

    Criando Models/connection e o index

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+const FAIL = 500;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
