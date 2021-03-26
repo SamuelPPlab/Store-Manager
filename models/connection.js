2322c28a9ee511c45c15d510c810b97d10ef1eea
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-03-26 18:49:09 -0300

    oh well 3

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
-// const MONGO_DB_nURL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_nURL = 'mongodb://mongodb:27017/StoreManager';
 
 const connection = () => {
   return MongoClient
