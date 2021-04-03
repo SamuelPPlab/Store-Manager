71157fee77bb25eaf744a59203ee6b05990abb96
deko possas <andrepossaster@gmail.com>
2021-04-03 01:19:58 -0300

    validade quant feita

--- service/salesService.js
+++ service/salesService.js
@@ -2,23 +2,17 @@ const MIN_QUANTITY = 0;
 const UNPROCESSABLE = 422;
 
 const validateQuantity = async (req, res, next) => {
-  const { quantity } = req.body;
-  if (!quantity || quantity <= MIN_QUANTITY) {
-    return res.status(UNPROCESSABLE).json({
-      err: {
-        code: 'invalid_data',
-        message: 'Wrong product ID or invalid quantity'
-      }
-    });
-  }
-  if (typeof quantity !== 'number'){
-    return res.status(UNPROCESSABLE).json({
-      err: {
-        code: 'invalid_data',
-        message: 'Wrong product ID or invalid quantity'
-      }
-    });
-  }
+  req.body.forEach((item) => {
+    const { quantity } = item;
+    if (!quantity || quantity <= MIN_QUANTITY || typeof quantity !== 'number') {
+      return res.status(UNPROCESSABLE).json({
+        err: {
+          code: 'invalid_data',
+          message: 'Wrong product ID or invalid quantity'
+        }
+      });
+    }
+  });
   next();
 };
 
