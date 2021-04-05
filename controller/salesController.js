d79c88efcbd06d1e1da7273cf942dbcba421e493
deko possas <andrepossaster@gmail.com>
2021-04-05 12:07:41 -0300

    controler de resposta

--- controller/salesController.js
+++ controller/salesController.js
@@ -1,9 +1,13 @@
 const SUCCESS = 200;
+const NOT_FOUND = 404;
 const { Router } = require('express');
 
 const {
   validateSaleQuantity,
   validateProductId,
+  validateSaleId,
+  getAllSales,
+  getSaleById,
 } = require('../service/salesService');
 const { salesProduct } =require('../models/saleModel');
 
@@ -17,6 +21,18 @@ Sales.post('/', validateSaleQuantity, validateProductId, async (req, res) => {
 Sales.get('/', async (_req, res) => {
   const allSales = await getAllSales();
   return res.status(SUCCESS).json({ sales: allSales });
+});
+
+Sales.get('/:id', validateSaleId, async ( req, res) => {
+  const { id } = req.params;
+  const saleById = await getSaleById(id);
+  if (!saleById) return res.status(NOT_FOUND).json({
+    err: {
+      code: 'not_found',
+      message: 'Sale not found',
+    }
+  });
+  return res.status(SUCCESS).json(saleById);
 });
 
 module.exports = { Sales };
