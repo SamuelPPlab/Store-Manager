109ebab53f736e6544435a292e49a7402dfd14b7
danilocouto1 <dansantosalfa@gmail.com>
2021-04-05 18:40:27 -0300

    fix resolution

--- model/connection.js
+++ model/connection.js
@@ -1,19 +1,15 @@
 const { MongoClient } = require('mongodb');
 
-// const MONGODB_URL = 'mongodb://127.0.0.1:27017';
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
-const connection = () => {
-  return MongoClient.connect(MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-    .then((conn) => conn.db(DB_NAME))
-    .catch((err) => {
-      console.error(err);
-      process.exit();
-    });
-};
+const connection = () => MongoClient.connect(MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((conn) => conn.db(DB_NAME))
+  .catch((_err) => {
+    process.exit();
+  });
 
 module.exports = connection;
